54dd01c0ff740890b40709fcf56d25f777827324
Parseval <igwee3333@gmail.com>
2023-12-21 08:26:08 +0100

    chore: added components

---
+++ components/features.tsx
@@ -0,0 +1,49 @@
+import { Bike, Star, Webhook } from "lucide-react";
+
+const Features = () => {
+  return (
+    <div className="flex flex-col items-center w-full space-y-8 p-10">
+      <h2 className="font-semibold text-primary">
+        Making Delivery <span className="uppercase text-[#A4C5D0]">easy</span>{" "}
+        for you
+      </h2>
+      <div className="flex space-x-8 justify-center">
+        <div className="flex flex-col space-y-8 max-w-lg">
+          <div className="flex flex-col space-y-4 w-fit">
+            <Star color="#A4C5D0" className="h-10 w-10" />
+            <p className="font-extrabold text-xl">First Mover Advantage</p>
+          </div>
+          <p className="font-medium max-w-md">
+            Providing a distinct first-mover advantage in underdeveloped markets
+            positioning our clients at the forefront of emerging opportunities
+            in these untapped regions
+          </p>
+        </div>
+        <div className="flex flex-col space-y-8 max-w-lg">
+          <div className="flex flex-col space-y-4">
+            <Bike color="#A4C5D0" className="h-10 w-10" />
+            <p className="font-extrabold text-xl">First Mover Advantage</p>
+          </div>
+          <p className="font-medium max-w-md">
+            Offers a versatile fleet that caters to a wide array of
+            transportation needs, ensuring flexibility and efficiency in meeting
+            the diverse requirements of our valued clients
+          </p>
+        </div>
+        <div className="flex flex-col space-y-8 max-w-lg">
+          <div className="flex flex-col space-y-4">
+            <Webhook color="#A4C5D0" className="h-10 w-10" />
+            <p className="font-extrabold text-xl">First Mover Advantage</p>
+          </div>
+          <p className="font-medium max-w-md">
+            Seamlessly integrating cutting-edge technology to ensure the highest
+            standards of efficiency and reliability in our services, setting a
+            new benchmark for the industry
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Features;
---
+++ components/services.tsx
@@ -0,0 +1,43 @@
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { ClipboardList, Package, Plane } from "lucide-react";
+
+const Services = () => {
+  return (
+    <div className="flex flex-col items-center space-y-14 p-10">
+      <h3 className="font-bold text-4xl">Uniform Delivery Services</h3>
+      <div className="flex space-x-12">
+        <Card className="shadow-xl rounded-3xl h-fit py-4">
+          <CardHeader className="flex flex-col space-y-8 justify-center items-center">
+            <Package className="h-20 w-20" />
+            <p className="font-bold text-4xl">Ship Now</p>
+          </CardHeader>
+          <CardContent className="font-normal text-sm text-center">
+            Request Pick-up, Delivery or Express Dropoff
+          </CardContent>
+        </Card>
+        <Card className="shadow-xl rounded-3xl">
+          <CardHeader className="flex flex-col space-y-8 justify-center items-center">
+            <Plane className="h-20 w-20" />
+            <p className="font-bold text-4xl w-1/2 text-center leading-relaxed">
+              International Shipping
+            </p>
+          </CardHeader>
+          <CardContent className="font-normal text-sm text-center">
+            Request Pick-up, Delivery or Express Dropoff
+          </CardContent>
+        </Card>
+        <Card className="shadow-xl rounded-3xl h-fit py-4">
+          <CardHeader className="flex flex-col space-y-8 justify-center items-center">
+            <ClipboardList className="h-20 w-20" />
+            <p className="font-bold text-4xl">Get a Quote</p>
+          </CardHeader>
+          <CardContent className="font-normal text-sm text-center">
+            Request Pick-up, Delivery or Express Dropoff
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
+};
+
+export default Services;
